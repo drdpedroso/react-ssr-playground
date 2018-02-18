123e02f932f4aef23631122e44dcaf7382289558
Eduardo Pedroso <drdpedroso@gmail.com>
2018-02-18 17:19:22 -0300

    Creating bundle for client side application

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 
 const Home = _ => {
-    return <div>I'm the BEST home componenet</div>
+    return (
+        <div>
+            <div>I'm the BEST home componenet</div>
+            <button onClick={_ => console.log('lek')}> Press me </button>
+        </div>
+    )
 }
 
 export default Home
